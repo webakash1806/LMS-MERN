a8a847da0f17b3a1f6f2bec9adfc8c7cdb75363d
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-10-13 19:33:52 +0530

    Cloudinary configuration done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -42,6 +42,7 @@ const register = async (req, res, next) => {
     }
 
     if (req.file) {
+        // console.log(req.file)
         try {
             const result = await cloudinary.v2.uploader.upload(req.file.path, {
                 folder: 'lms',
@@ -53,10 +54,12 @@ const register = async (req, res, next) => {
             if (result) {
                 user.avatar.public_id = result.public_id
                 user.avatar.secure_url = result.secure_url
+
+                fs.rm(`uploads/${req.file.filename}`)
             }
         }
         catch (err) {
-
+            return next(new AppError('Some error occured', 500))
         }
     }
 
