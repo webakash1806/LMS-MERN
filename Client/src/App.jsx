bd8edfdac1190c19db10b43ff331517220695c1d
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-12-25 14:36:34 +0530

    Configurred eslint sorting

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className=''>
-      LMS Client
-    </div>
+    <>
+      <Routes>
+        {/* <Route path="./" element={<Home/>} /> */}
+      </Routes>
+    </>
   )
 }
 
