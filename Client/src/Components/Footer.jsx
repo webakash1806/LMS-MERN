ea169b54d7cb3d6cbc3328646a7c42e60c06b3e5
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-12-28 09:51:11 +0530

    Footer responsive

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -12,59 +12,61 @@ import {
 const Footer = () => {
     return (
         <>
-            <footer className=' bg-[#1B2124] text-white p-[2.5rem_1rem] flex flex-col gap-8'>
-                <div className='flex flex-col gap-2'>
-                    <div className='mb-3'>LOGO</div>
-                    {/* <img src="" alt="" /> */}
-                    <a href="" className='flex items-center gap-2 text-[0.83rem] font-[400] tracking-wide'><BsEnvelope /><span>itsakash18.06@gmail.com</span></a>
-                    <a href="" className='flex items-center gap-2 text-[0.83rem] font-[400] tracking-wide'><BsTelephone /><span>+91 6207234759</span></a>
-                    <div className='flex gap-6 mt-3'>
-                        <a href="" className='text-[17px]'><BsLinkedin /></a>
-                        <a href="" className='text-[17px]'><BsFacebook /></a>
-                        <a href="" className='text-[17px]'><BsWhatsapp /></a>
-                        <a href="" className='text-[17px]'><BsInstagram /></a>
-                        <a href="" className='text-[17px]'><BsTwitter /></a>
+            <footer className=' bg-[#1B2124] text-white'>
+                <div className='p-[2.5rem_1rem] md:p-[2.5_5rem] flex flex-col gap-8 lg:flex-row sm:justify-around'>
+                    <div className='flex flex-col gap-2'>
+                        <div className='mb-3'>LOGO</div>
+                        {/* <img src="" alt="" /> */}
+                        <a href="" className='flex items-center gap-2 text-[0.83rem] font-[400] tracking-wide'><BsEnvelope /><span>itsakash18.06@gmail.com</span></a>
+                        <a href="" className='flex items-center gap-2 text-[0.83rem] font-[400] tracking-wide'><BsTelephone /><span>+91 6207234759</span></a>
+                        <div className='flex gap-6 mt-3'>
+                            <a href="" className='text-[17px]'><BsLinkedin /></a>
+                            <a href="" className='text-[17px]'><BsFacebook /></a>
+                            <a href="" className='text-[17px]'><BsWhatsapp /></a>
+                            <a href="" className='text-[17px]'><BsInstagram /></a>
+                            <a href="" className='text-[17px]'><BsTwitter /></a>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-8 sm:flex-row sm:justify-around sm:mr-5 md:mr-[5rem] md:gap-[5rem]'>
+                        <div className='w-[17rem] '>
+                            <div>
+                                <p className='text-[1.09rem] font-[600]'>Company</p>
+                                <p className='w-[15rem] m-[9px_0] h-[3.4px] bg-[#ff9500] rounded-md'></p>
+                            </div>
+                            <div className='mt-6 flex gap-4 justify-between pr-6'>
+                                <div className='flex flex-col gap-4 '>
+                                    <p className="text-[0.82rem] text-slate-300">About Us</p>
+                                    <p className="text-[0.82rem] text-slate-300">FAQ</p>
+                                    <p className="text-[0.82rem] text-slate-300">Privacy policy</p>
+                                </div>
+                                <div className='flex flex-col gap-4'>
+                                    <p className="text-[0.82rem] text-slate-300">Contact us</p>
+                                    <p className="text-[0.82rem] text-slate-300">Job assurance</p>
+                                    <p className="text-[0.82rem] text-slate-300">Terms & condition</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-[17rem] '>
+                            <div>
+                                <p className='text-[1.09rem] font-[600]'>Products</p>
+                                <p className='w-[15rem] m-[9px_0] h-[3.3px] bg-[#ff9500] rounded-md'></p>
+                            </div>
+                            <div className='mt-6 flex gap-4 justify-between pr-6'>
+                                <div className='flex flex-col gap-4'>
+                                    <p className="text-[0.82rem] text-slate-300">PW Skills Lab</p>
+                                    <p className="text-[0.82rem] text-slate-300">Experience Portal</p>
+                                    <p className="text-[0.82rem] text-slate-300">Hall Of fame</p>
+                                </div>
+                                <div className='flex flex-col gap-4'>
+                                    <p className="text-[0.82rem] text-slate-300">Job Portal</p>
+                                    <p className="text-[0.82rem] text-slate-300">Become an affiliate</p>
+                                    <p className="text-[0.82rem] text-slate-300">Blog</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='flex flex-col gap-8'>
-                    <div>
-                        <div>
-                            <p className='text-[1.09rem] font-[600]'>Company</p>
-                            <p className='w-[15rem] m-[9px_0] h-[3.4px] bg-[#ff9500] rounded-md'></p>
-                        </div>
-                        <div className='mt-6 flex gap-4 justify-between pr-6'>
-                            <div className='flex flex-col gap-4 '>
-                                <p className="text-[0.82rem] text-slate-300">About Us</p>
-                                <p className="text-[0.82rem] text-slate-300">FAQ</p>
-                                <p className="text-[0.82rem] text-slate-300">Privacy policy</p>
-                            </div>
-                            <div className='flex flex-col gap-4'>
-                                <p className="text-[0.82rem] text-slate-300">Contact us</p>
-                                <p className="text-[0.82rem] text-slate-300">Job assurance</p>
-                                <p className="text-[0.82rem] text-slate-300">Terms & condition</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <p className='text-[1.09rem] font-[600]'>Products</p>
-                            <p className='w-[15rem] m-[9px_0] h-[3.3px] bg-[#ff9500] rounded-md'></p>
-                        </div>
-                        <div className='mt-6 flex gap-4 justify-between pr-6'>
-                            <div className='flex flex-col gap-4'>
-                                <p className="text-[0.82rem] text-slate-300">PW Skills Lab</p>
-                                <p className="text-[0.82rem] text-slate-300">Experience Portal</p>
-                                <p className="text-[0.82rem] text-slate-300">Hall Of fame</p>
-                            </div>
-                            <div className='flex flex-col gap-4'>
-                                <p className="text-[0.82rem] text-slate-300">Job Portal</p>
-                                <p className="text-[0.82rem] text-slate-300">Become an affiliate</p>
-                                <p className="text-[0.82rem] text-slate-300">Blog</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+                <div className='text-center text-[1.05rem] font-[600] p-3 border-t'>2024 <span>&#169;</span> | LMS</div>
             </footer>
         </>
     )
