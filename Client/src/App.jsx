46bfbc70a4af0ad8d6df74d044f08b1eaecb83a4
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-01-01 15:23:06 +0530

    404 Error page created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import AboutUs from './Pages/AboutUs'
 import HomePage from './Pages/HomePage'
+import PageNotFound from './Pages/PageNotFound'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/LMS-Client" element={<HomePage />} />
         <Route path="/LMS-Client/about" element={<AboutUs />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   )
---
+++ Client/src/Pages/PageNotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import Error404 from '../assets/Error404.png'
+
+const PageNotFound = () => {
+    const navigate = useNavigate()
+    return (
+        <div className='bg-[#eaffff] w-screen md:flex-row-reverse h-screen flex flex-col items-center justify-center'>
+            <img className='w-[19rem] sm:w-[22rem] lg:w-[26rem]' src={Error404} alt="404 Page not found Image!" />
+            <div className='flex flex-col items-center justify-center'>
+                <p className='text-[#000] font-bold text-[1.5rem] text-center mx-8'>Looks Like you&#39;re lost the address!</p>
+                <button onClick={() => navigate(-1)} className='mt-4 bg-[#FFB827] text-black p-[5px] px-4 font-semibold rounded-md animate-bounce shadow-sm hover:bg-black hover:text-[#FFB827]'>Go Back!</button>
+            </div>
+        </div>
+    )
+}
+
+export default PageNotFound
