66d942bc0f1a2d19194b856a15de18588278193b
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-01-11 08:05:03 +0530

    Lecture slice created

---
+++ Client/src/Redux/Slices/LectureSlice.js
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import toast from "react-hot-toast"
+
+import axiosInstance from "../../Helpers/axiosInstance"
+
+const initialState = {
+    lectures: []
+}
+
+export const getCourseLectures = createAsyncThunk("/course/lectures", async (courseId) => {
+    try {
+        const response = await axiosInstance.get(`/course/${courseId}`)
+        toast.promise(response, {
+            loading: "Loading lectures",
+            success: "Lectures loaded successfully",
+            error: "Failed to load the lectures"
+        })
+
+        return (await response).data
+
+    } catch (e) {
+        return toast.error(e?.response?.data?.message)
+    }
+})
+
+export const addCourseLectures = createAsyncThunk("/course/create/lectures", async (data) => {
+    try {
+        const response = await axiosInstance.post(`/course/create/lectures/${data[0]}`, data[1])
+        toast.promise(response, {
+            loading: "Adding lecture",
+            success: "Lectures added successfully",
+            error: "Failed to add the lectures"
+        })
+
+        return (await response).data
+
+    } catch (e) {
+        return toast.error(e?.response?.data?.message)
+    }
+})
+
+export const deleteCourseLecture = createAsyncThunk("/course/remove/lectures", async (data) => {
+    try {
+        const response = await axiosInstance.delete(`/course/remove/lectures/${data[0]}`, data[1])
+        toast.promise(response, {
+            loading: "Removing lecture",
+            success: "Lectures removed successfully",
+            error: "Failed to remove the lectures"
+        })
+
+        return (await response).data
+
+    } catch (e) {
+        return toast.error(e?.response?.data?.message)
+    }
+})
+
+const lectureSlice = createSlice({
+    name: "lecture",
+    initialState,
+    reducers: {},
+    extraReducers: () => { }
+})
+
+export default lectureSlice.reducer
--- Client/src/Redux/store.js
+++ Client/src/Redux/store.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import authSliceReducer from './Slices/AuthSlice'
 import courseSliceReducer from './Slices/CourseSlice'
+import LectureSlice from './Slices/LectureSlice'
 import RazorpaySlice from './Slices/RazorpaySlice'
 
 const store = configureStore({
     reducer: {
         auth: authSliceReducer,
         course: courseSliceReducer,
-        razorpay: RazorpaySlice
+        razorpay: RazorpaySlice,
+        lecture: LectureSlice
     },
     devTools: true
 })
