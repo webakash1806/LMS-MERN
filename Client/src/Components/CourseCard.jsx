af2c14ac53eda26a582d4ba24e1cd81544fd14ad
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-01-20 18:04:01 +0530

    Home page updated

--- Client/src/Components/CourseCard.jsx
+++ Client/src/Components/CourseCard.jsx
@@ -12,7 +12,7 @@ const CourseCard = ({ data }) => {
         <div className='w-[17.5rem] h-[27rem] cursor-pointer flex mt-2 flex-col border items-start gap-2 rounded-md overflow-hidden'
             onClick={() => navigate("/LMS-Client/course/description", { state: { ...data } })}>
             <img className='w-full h-[40%] object-cover' src={data?.thumbnail?.secure_url} alt={`${data?.title} image`} />
-            <div className='text-white flex flex-col gap-2 p-3'>
+            <div className='text-white flex flex-col gap-2 p-3 w-full'>
                 <h2 className='text-[1.35rem] font-[600] capitalize'>{data?.title}</h2>
                 <p className='text-[0.9rem] line-clamp-2'>{data?.description}</p>
                 <div className='flex flex-wrap gap-2 mt-1'>
@@ -20,10 +20,6 @@ const CourseCard = ({ data }) => {
                     <div className='border w-fit p-1 px-2 rounded-full text-[0.95rem] flex gap-2 items-center '><BsBook /><span className='text-[0.7rem] tracking-wide'>{data?.category}</span></div>
                     <div className='border w-fit p-1 px-2 rounded-full text-[0.95rem] flex gap-2 items-center '><BsCameraVideo /><span className='text-[0.7rem] tracking-wide'>Total Lectures : {data?.numberOfLecture}</span></div>
                 </div>
-                {/* <div>
-                    <div></div>
-                    <div></div>
-                </div> */}
             </div>
         </div>
     )
--- Client/src/Pages/HomePage.jsx
+++ Client/src/Pages/HomePage.jsx
@@ -6,51 +6,78 @@ const HomePage = () => {
 
     return (
         <HomeLayout>
-            <main className='text-white min-h-[90vh] py-8  md:pr-8 lg:pr-12 sm:py-4 flex flex-col-reverse md:flex-row items-center justify-center md:justify-around'>
-                <div className='p-3 px-5 text-center md:text-left md:w-[27rem] lg:w-[37rem]'>
-                    <h1 className='text-[2.2rem] md:text-[2.4rem] lg:text-[2.6rem] font-semibold flex flex-col lg:gap-3 lg:flex-row'>
-                        Find Out Best
-                        <span className='text-yellow-400'>
-                            Online Courses
-                        </span>
-                    </h1>
-                    <p className='font-[330] text-[0.97rem] py-3 tracking-[0.3px] lg:pb-8'>
-                        Explore diverse coding courses, master programming languages, and build real-world projects with expert-led online tutorials for comprehensive skill development.
-                    </p>
+            <div>
+                <main className='text-white min-h-[90vh] py-8  md:pr-8 lg:pr-12 sm:py-4 flex flex-col-reverse md:flex-row items-center justify-center md:justify-around w-full bg-gradient-to-b  overflow-x-hidden from-[#15191E] via-[#320a17cb]   to-[#4b031b]'>
+                    <div className='p-3 px-5 text-center md:text-left md:w-[27rem] lg:w-[37rem]'>
+                        <h1 className='text-[2.2rem] md:text-[2.4rem] lg:text-[2.6rem] font-semibold flex flex-col lg:gap-3 lg:flex-row'>
+                            Find Out Best
+                            <span className='text-yellow-400'>
+                                Online Courses
+                            </span>
+                        </h1>
+                        <p className='font-[400] text-[0.97rem] py-3 tracking-[0.4px] lg:pb-8'>
+                            Explore diverse coding courses, master programming languages, and build real-world projects with expert-led online tutorials for comprehensive skill development.
+                        </p>
+                        <div>
+                            <Link to='/LMS-Client/course'>
+                                <button className='bg-[#7479ff] p-2 px-8 font-[500] tracking-wide rounded-md text-[1.05rem] hover:bg-[#6368fa]'>
+                                    Explore Courses
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
                     <div>
-                        <Link to='/LMS-Client/course'>
-                            <button className='bg-yellow-600 p-2 px-8 font-[500] tracking-wide rounded-md text-[1.05rem] hover:bg-yellow-700'>
-                                Explore Courses
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-                <div>
-                    <div className="w-64 carousel rounded-box" >
-                        <div className="carousel-item w-full">
-                            <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
-                        </div>
-                        <div className="carousel-item w-full">
-                            <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
-                        </div>
-                        <div className="carousel-item w-full">
-                            <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
-                        </div>
-                        <div className="carousel-item w-full">
-                            <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
-                        </div>
-                        <div className="carousel-item w-full">
-                            <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
-                        </div>
-                        <div className="carousel-item w-full">
-                            <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
-                        </div>
-                        <div className="carousel-item w-full">
-                            <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        <div className="w-64 carousel rounded-box" >
+                            <div className="carousel-item w-full">
+                                <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                            </div>
+                            <div className="carousel-item w-full">
+                                <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                            </div>
+                            <div className="carousel-item w-full">
+                                <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                            </div>
+                            <div className="carousel-item w-full">
+                                <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                            </div>
+                            <div className="carousel-item w-full">
+                                <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                            </div>
+                            <div className="carousel-item w-full">
+                                <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                            </div>
+                            <div className="carousel-item w-full">
+                                <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </main>
+                </main>
+                <section>
+                    <div className='w-full p-5 py-12 sm:px-20 md:px-[10vw] lg:px-[25vw]'>
+                        <div className=" join join-vertical w-full rounded-md shadow-[2px_2px_9px_#000,-2px_-2px_1px_#3a3b3a]">
+                            <div className="collapse collapse-arrow join-item border border-[#2d3a4b] bg-[#1A202A]">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    Is the course live or recorded?
+                                </div>
+                                <div className="collapse-content text-white">
+                                    <p>All lectures are recorded.</p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-arrow join-item border border-[#2d3a4b] bg-[#1A202A]">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    Do the courses start from the basics ?
+                                </div>
+                                <div className="collapse-content text-white">
+                                    <p>Yes every course starts from the very basics until it is specifically mentioned otherwise.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </section>
+            </div>
         </HomeLayout>
     )
 }
